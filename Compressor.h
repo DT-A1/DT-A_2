92ccd7a6e2d5475c93c7a1527eaeb0f1e908e8e7
trung16861 <trung16861@users.noreply.github.com>
2019-12-09 20:40:21 +0700

    Update Compressor.h

--- Compressor.h
+++ Compressor.h
@@ -7,16 +7,22 @@
 using namespace std;
 class Compressor
 {
-	//fileName
+private:
 	Coding::Node* root;
 	string inputFile;
 	string outputFile;
+	bool done;
+	string bodyBuffer;
+	string headBuffer;
+	void createHeader();
+	void writeTree(Coding::Node* root);
+	void addToBuffer(string& bits, FILE* outFile);
 public:
 	void SetInputFile(string inputFile);
 	void SetOutputFile(string outputFile);
+	//void setPseudoEOF(unordered_map<char, string> &codes);
 	void compress();
-	void decompress();
-	Compressor();
+	Compressor(string inputFileName, string outputFileName);
 	~Compressor();
 };
 
