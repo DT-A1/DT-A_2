fbc5ce79cacf4190f49b222da05fcb007e2203c6
trung16861 <trung16861@users.noreply.github.com>
2019-12-09 20:40:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
+#include "Compressor.h"
+#include "Extractor.h"
 using namespace std;
 
 void main()
 {
+	int choice;
+	while (1)
+	{
+		cout << "Compress file (0) or extrat file(1): ";
+		cin >> choice;
+		if (choice == 0)
+		{
+			string fileName, cprFileName;
+			cout << "Input file's name: ";
+			cin.ignore();
+			getline(cin, fileName);
+			cout << "Input compress file's name: ";
+			getline(cin, cprFileName);
+			Compressor cpr(fileName, cprFileName);
+			try {
+				cpr.compress();
+				cout << "Compression done!" << endl;
+			}
+			catch (const char* err)
+			{
+				cout << err << endl;
+			}
+		}
+		else
+		{
+			string fileName;
+			cout << "Input file's name: ";
+			cin.ignore();
+			getline(cin, fileName);
+			Extractor ext(fileName);
+			try {
+				ext.extract();
+				cout << "Extraction done!" << endl;
+			}
+			catch (const char* err)
+			{
+				cout << err << endl;
+			}
+		}
+	}
 	cin.get();
 }
