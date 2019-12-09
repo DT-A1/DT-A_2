304cf3625c73ac612a0f80b0faf6db77b030b3c1
trung16861 <trung16861@users.noreply.github.com>
2019-12-09 20:40:10 +0700

    Create Extractor.h

---
+++ Extractor.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <bitset>
+#include "Coding.h"
+class Extractor
+{
+private:
+	struct Node {
+		char ch;
+		Node* left;
+		Node* right;
+	};
+	Node* codeTree;
+	string inputFile;
+	string outputFile;
+	int endBlanks;
+	int nameLength;
+	bool done;
+	int fileSize;
+	int nCharLeft;
+	string bitsBuffer;
+	string charsBuffer;
+	void readBits(FILE*& inFile);
+	string readFileName(FILE*& inFile);
+	Node* readTree(FILE*& inFile);
+	char decodeBits(Node* codeTree, int index);
+	char getNextChar(FILE*& inFile);
+public:
+	void extract();
+	Extractor(string inputFileName);
+	~Extractor();
+};
+
